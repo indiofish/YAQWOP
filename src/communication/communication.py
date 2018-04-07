0ba419bfdf23a7ebf65cebb9073548a86280f1d2
thoum <indiofish@gmail.com>
2018-04-07 14:13:54 +0900

    runs() now only accept only one sequence of list of sequences

--- src/communication/communication.py
+++ src/communication/communication.py
@@ -1,19 +1,14 @@
 # Give a sequence of QWOPs to QWOPPER, and get the result and print
 import sys
-
 sys.path.append("../../dist/qwopper.jar")
-import com.slowfrog.qwop.ui.Main as TEST
-
+import com.slowfrog.qwop.ui.Main as qwopper
 
 
 class Communicator(object):
     def __init__(self):
-        TEST.main([])
+        qwopper.main([])
 
-    def run(self, sequences):
-        ret = []
-        for s in sequences:
-            info = TEST.playOneGame(s)
-            ret.append((info.distance, info.duration))
+    def run(self, sequence):
+        info = qwopper.playOneGame(sequence)
 
-        return ret
+        return (info.distance, info.duration)
